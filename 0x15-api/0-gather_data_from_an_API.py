cba012d38fcb0b476e7763ee267d0ac05ba621b6
elvis wangari <95751917+elviswangari@users.noreply.github.com>
2023-04-26 16:50:37 +0300

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
             done_tasks.append(task)
             done += 1
 
-    print("Employee {} is done with task ({}/{}):"
-          .format(employeeName, done, len(tasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
-          print("\t {}".format(task.get('title'))
+        print("\t {}".format(task.get('title')))
